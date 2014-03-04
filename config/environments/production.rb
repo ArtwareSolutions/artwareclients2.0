eb84a763d2a309c56b2f7054c012ea9acb412218
Simon Letort <simon@artwaresolutions.org>
2014-03-04 01:03:56 -0300

    trying to remove secrets.yml from git

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+# saved as environment variable on Heroku. variable declaration done in application.yml as secrets.yml is in .gitignore
+  :secret_key_base => ENV['SECRET_KEY_BASE']
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
